67b3d86c48008945ef2023154374ff44902666ae
joker69216 <hamaraad883@gmail.com>
2022-10-04 17:55:56 +0300

    Fix eslinter errors

--- src/js/todo.js
+++ src/js/todo.js
@@ -87,9 +87,10 @@ addInput.addEventListener('keypress', (e) => {
 
 clearBtn.addEventListener('click', () => {
   todos = todos.filter((todo) => todo.completed !== true);
+  /* eslint-disable prefer-const */
   let i = 0;
   todos.forEach((todo) => {
-    todo.index = i+1;
+    todo.index = i + 1;
   });
   index = todos.length;
   localStorage.setItem('todos', JSON.stringify(todos));
